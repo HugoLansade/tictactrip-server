8fd1e623795208ec1e4b05ea2662d2c314742f14
Hugo Lansade <lhugo@lansade.com>
2022-01-22 11:56:43 +0100

    modif for deploy

--- app.js
+++ app.js
@@ -11,6 +11,20 @@ app.use(express.text());
 let text = require("./routes/text.js")
 app.use("/api/justify", text);
 
+app.use(express.static(path.join(__dirname, "public/build")));
+
+app.use("/api/*", (req, res, next) => {  
+    const error = new Error("Ressource not found.");
+    error.status = 404;
+    next(error);
+  });
+
+  if (process.env.NODE_ENV === "production") {
+    app.use("*", (req, res, next) => {
+      // If no routes match, send them the React HTML.
+      res.sendFile(path.join(__dirname, "public/build/index.html"));
+    });
+  }
 
 app.listen(process.env.PORT_TEXT, function(err){
     if (err) console.log(err);
